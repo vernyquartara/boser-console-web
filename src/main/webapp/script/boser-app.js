e7f4d7f2eb4500a13a18bad4b275db09936906bc
verny <webny23@gmail.com>
2016-05-09 22:28:10 +0200

    configurazione latency per la barra di caricamento

--- src/main/webapp/script/boser-app.js
+++ src/main/webapp/script/boser-app.js
@@ -7,5 +7,6 @@ angular.module("Boser", [
 .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
     cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
     cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner">Il crawler si sta avviando, per favore attendi ...</div>';
+    cfpLoadingBarProvider.latencyThreshold = 1000;
   }]);
  
